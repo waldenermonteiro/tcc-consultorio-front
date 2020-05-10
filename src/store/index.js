47baf9298642a21916e536f65470bbd8f2f452b5
Waldener Monteiro <jjrr019@gmail.com>
2020-05-09 22:47:15 -0300

    refactor: adicionando rotas e stores

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import Profiles from '../pages/profiles/store'
+import Specialities from '../pages/specialities/store'
+import TypesExams from '../pages/types_exams/store'
 Vue.use(Vuex)
 
 export default function (/* { ssrContext } */) {
   const Store = new Vuex.Store({
     modules: {
-      Profiles
+      Profiles,
+      Specialities,
+      TypesExams
     },
 
     // enable strict mode (adds overhead!)
