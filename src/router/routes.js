2e8f72ef3ae0b009991709f0d079668be1c5b138
Waldener Monteiro <jjrr019@gmail.com>
2020-05-09 21:10:22 -0300

    refactor: preparando a rota de profiles com o name e o icon

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,10 +1,10 @@
-
 const routes = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Index.vue') }
+      { path: '', component: () => import('pages/Index.vue') },
+      { path: '/perfis', component: () => import('pages/profiles/List.vue'), meta: { name: 'Configurações / Perfis', icons: 'settings / person' } }
     ]
   }
 ]
