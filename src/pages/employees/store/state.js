0126a50cfeecd052aaded2b3892486e1532193a5
Waldener Monteiro <jjrr019@gmail.com>
2020-05-13 04:39:43 -0300

    refactor(employee): apresentando o perfil na listagem

--- src/pages/employees/store/state.js
+++ src/pages/employees/store/state.js
@@ -6,6 +6,7 @@ export default function () {
       { name: 'name', align: 'center', label: 'Nome', field: 'name', sortable: true },
       { name: 'user', align: 'center', label: 'Email', field: 'user', sortable: true },
       { name: 'specialitie', align: 'center', label: 'Especialidade', field: 'specialitie', sortable: true },
+      { name: 'profile', align: 'center', label: 'Perfil', field: 'profile', sortable: true },
       { name: 'actions', align: 'center', label: 'Ações', field: 'actions', sortable: true, headerStyle: 'width: 100px' }
     ]
   }
