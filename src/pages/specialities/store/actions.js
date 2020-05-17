b7519d53fa02fca8d58993aebe3a64f9d8ddcb8f
Waldener Monteiro <jjrr019@gmail.com>
2020-05-17 19:13:09 -0300

    refactor: adicionando regra nas actions para desabilitar erro de eslint

--- src/pages/specialities/store/actions.js
+++ src/pages/specialities/store/actions.js
@@ -1,31 +1,31 @@
 import SpecialitiesService from '../services/SpecialitiesService'
-
+/* eslint-disable no-useless-catch */
 export async function list ({ commit }) {
   try {
     const { data } = await SpecialitiesService.list()
     commit('LIST', data)
   } catch (error) {
-    return error
+    throw error
   }
 }
 export async function create ({ commit }, params) {
   try {
     await SpecialitiesService.create(params)
   } catch (error) {
-    return error
+    throw error
   }
 }
 export async function update ({ commit }, params) {
   try {
     await SpecialitiesService.update(params, params.id)
   } catch (error) {
-    return error
+    throw error
   }
 }
 export async function remove ({ commit }, params) {
   try {
     await SpecialitiesService.remove(params.id)
   } catch (error) {
-    return error
+    throw error
   }
 }
--- src/pages/types_exams/store/actions.js
+++ src/pages/types_exams/store/actions.js
@@ -1,31 +1,31 @@
 import TypesExams from '../services/TypesExamsService'
-
+/* eslint-disable no-useless-catch */
 export async function list ({ commit }) {
   try {
     const { data } = await TypesExams.list()
     commit('LIST', data)
   } catch (error) {
-    return error
+    throw error
   }
 }
 export async function create ({ commit }, params) {
   try {
     await TypesExams.create(params)
   } catch (error) {
-    return error
+    throw error
   }
 }
 export async function update ({ commit }, params) {
   try {
     await TypesExams.update(params, params.id)
   } catch (error) {
-    return error
+    throw error
   }
 }
 export async function remove ({ commit }, params) {
   try {
     await TypesExams.remove(params.id)
   } catch (error) {
-    return error
+    throw error
   }
 }
