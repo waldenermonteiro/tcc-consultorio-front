7f22bdf103dabf90ca25a3a1cc656aa2505703d2
Waldener Monteiro <jjrr019@gmail.com>
2020-05-20 05:27:23 -0300

    refactor: adicionando token jwt no header

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -2,23 +2,24 @@ import Vue from 'vue'
 import axios from 'axios'
 
 Vue.prototype.$axios = axios
-
+const token = window.localStorage.getItem('token_clinic')
+console.log(token)
 const HTTPClient = axios.create({
   baseURL: 'http://127.0.0.1:3333/api/v1/',
   headers: { 'Content-Type': 'application/json', 'Cache-Control': 'no-cache', Pragma: 'no-cache' }
 })
 
-// HTTPClient.interceptors.request.use(function (config) {
-//   return Vue.prototype.$mgr.getUser()
-//     .then((res) => {
-//       // config.headers.Authorization = `Bearer ${res.id_token}`
-//       return config
-//     }, (err) => {
-//       console.log(err)
-//     })
-// }, function (error) {
-//   return Promise.reject(error)
-// })
+HTTPClient.interceptors.request.use(
+  async config => {
+    if (!config.url.endsWith('login')) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  error => {
+    return Promise.reject(error)
+  }
+)
 
 Vue.prototype.$axios = HTTPClient
 
