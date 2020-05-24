b7ff68d349c70faab211a5454e1d02a6896c9ae4
Waldener Monteiro <jjrr019@gmail.com>
2020-05-24 04:06:13 -0300

    refactor(medical_schedules): criando os filtros restantes de paciente e data

--- src/pages/medical_schedules/components/ListFilter.vue
+++ src/pages/medical_schedules/components/ListFilter.vue
@@ -7,7 +7,35 @@
       </div>
       <div class="col-3">
         Médico:
-        <q-select outlined v-model="formFilter.employee_id" option-value="id" option-label="name" :options="employees" dense emit-value map-options  />
+        <q-select outlined v-model="formFilter.employee_id" option-value="id" option-label="name" :options="employees" dense emit-value map-options />
+      </div>
+      <div class="col-3">
+        Paciente*:
+        <q-select
+          outlined
+          :options="optionsPatients"
+          option-value="id"
+          option-label="name"
+          v-model="formFilter.patient_id"
+          dense
+          use-input
+          emit-value
+          map-options
+          @filter="filterPatient"
+        >
+        </q-select>
+      </div>
+      <div class="col-3">
+        Data*:
+        <q-input outlined v-model="formFilter.date_appointment" dense @click="$refs.qDateAppointment.show()">
+          <template v-slot:append>
+            <q-icon name="event" class="cursor-pointer">
+              <q-popup-proxy ref="qDateAppointment" transition-show="scale" transition-hide="scale">
+                <q-date color="primary" v-model="formFilter.date_appointment" mask="DD/MM/YYYY" />
+              </q-popup-proxy>
+            </q-icon>
+          </template>
+        </q-input>
       </div>
       <div class="col-7">
         <div class="row justify-center q-col-gutter-sm">
@@ -23,28 +51,57 @@
   </div>
 </template>
 <script>
-import ListValidator from '../mixins/ListValidator.js'
 import { mapState } from 'vuex'
 export default {
-  mixins: [ListValidator],
   data () {
-    return {}
+    return {
+      formFilter: {
+        name: '',
+        employee_id: '',
+        patient_id: '',
+        date_appointment: ''
+      },
+      formFilterCopy: { ...this.formFilter },
+      optionsPatients: this.patients
+    }
   },
   mounted () {
     this.$list({ urlDispatch: 'Employees/list' })
+    this.$list({ urlDispatch: 'Patients/list' })
   },
   computed: {
-    ...mapState('Employees', ['employees'])
+    ...mapState('Employees', ['employees']),
+    ...mapState('Patients', ['patients'])
   },
   methods: {
+    filterPatient (val, update, abort) {
+      update(() => {
+        if (val.length < 2) {
+          abort()
+          return ''
+        } else {
+          const needle = val.toLowerCase()
+          this.optionsPatients = this.patients.filter(v => v.name.toLowerCase().indexOf(needle) > -1)
+        }
+      })
+    },
+    prepareParams (form) {
+      const formCustom = { ...form }
+      formCustom.date_appointment = this.$formatDateBrInApi(this.formFilter.date_appointment)
+      return formCustom
+    },
     send () {
-      //   this.verifiyValidations()
-      this.$list({ urlDispatch: 'MedicalSchedules/list', params: this.formFilter })
+      const params = this.prepareParams(this.formFilter)
+      this.$list({ urlDispatch: 'MedicalSchedules/list', params })
     },
     clearForm () {
-      this.form = { ...this.formCopy }
-      this.$list({ urlDispatch: 'MedicalSchedules/list', params: this.formFilter })
-      //   this.$v.form.$reset()
+      this.formFilter = { ...this.formFilterCopy }
+      this.$list({
+        urlDispatch: 'MedicalSchedules/list',
+        callback: () => {
+          this.optionsPatients = this.patients
+        }
+      })
     }
   }
 }
