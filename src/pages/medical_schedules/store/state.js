145122e543a70abf6c12cda88accf7fd292296d1
Waldener Monteiro <jjrr019@gmail.com>
2020-05-31 04:39:00 -0300

    feature(medical_schedules): adicionando array de colunas da litagem de consultas do dia

--- src/pages/medical_schedules/store/state.js
+++ src/pages/medical_schedules/store/state.js
@@ -10,6 +10,14 @@ export default function () {
       { name: 'specialitie', align: 'center', label: 'Especialidade', field: 'specialitie', sortable: true },
       { name: 'status', align: 'center', label: 'Status', field: 'status', sortable: true },
       { name: 'actions', align: 'right', label: 'Ações', field: 'actions', sortable: true, headerStyle: 'width: 200px' }
+    ],
+    columnsListOfMedic: [
+      { name: 'id', align: 'center', label: 'Id', field: 'id', sortable: true },
+      { name: 'name', align: 'center', label: 'Descrição', field: 'name', sortable: true },
+      { name: 'date_appointment', align: 'center', label: 'Data da Consulta', field: 'date_appointment', sortable: true },
+      { name: 'patient', align: 'center', label: 'Paciente', field: 'patient', sortable: true },
+      { name: 'status', align: 'center', label: 'Status', field: 'status', sortable: true },
+      { name: 'actions', align: 'right', label: 'Ações', field: 'actions', sortable: true, headerStyle: 'width: 100px' }
     ]
   }
 }
