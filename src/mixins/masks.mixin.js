35feb2c3abfb03fdb2fe6369df5bb40f8053af5d
Waldener Monteiro <jjrr019@gmail.com>
2020-06-09 06:58:21 -0300

    refactor: retirando console.log

--- src/mixins/masks.mixin.js
+++ src/mixins/masks.mixin.js
@@ -57,7 +57,6 @@ export default {
     },
     $formatDateAndHourBr (date) {
       if (date === null || date === '') return ''
-      console.log(date)
       const pattern = /(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})/g
       const getDate = pattern.exec(date)[0]
       return getDate.replace(pattern, '$3/$2/$1 $4:$5:$6')
