0398919d9565219790558d1e8a44bc6ba046cd41
Waldener Monteiro <jjrr019@gmail.com>
2020-06-17 03:12:55 -0300

    refactor(medical_schedules): centralizando colunas das tabelas

--- src/pages/medical_schedules/store/state.js
+++ src/pages/medical_schedules/store/state.js
@@ -15,7 +15,7 @@ export default function () {
       { name: 'employee', align: 'center', label: 'Médico', field: 'employee', sortable: true },
       { name: 'specialitie', align: 'center', label: 'Especialidade', field: 'specialitie', sortable: true },
       { name: 'status', align: 'center', label: 'Status', field: 'status', sortable: true },
-      { name: 'actions', align: 'right', label: 'Ações', field: 'actions', headerStyle: 'width: 130px' }
+      { name: 'actions', align: 'center', label: 'Ações', field: 'actions', headerStyle: 'width: 130px' }
     ],
     columnsListOfMedic: [
       { name: 'id', align: 'center', label: 'Id', field: 'id', sortable: true },
@@ -23,13 +23,13 @@ export default function () {
       { name: 'date_appointment', align: 'center', label: 'Data da Consulta', field: 'date_appointment', sortable: true },
       { name: 'patient', align: 'center', label: 'Paciente', field: 'patient', sortable: true },
       { name: 'status', align: 'center', label: 'Status', field: 'status', sortable: true },
-      { name: 'actions', align: 'right', label: 'Ações', field: 'actions', headerStyle: 'width: 100px' }
+      { name: 'actions', align: 'center', label: 'Ações', field: 'actions', headerStyle: 'width: 100px' }
     ],
     columnsHistoryPatient: [
       { name: 'id', align: 'center', label: 'Id', field: 'id', sortable: true },
       { name: 'date_appointment', align: 'center', label: 'Data da Consulta', field: 'date_appointment', sortable: true },
       { name: 'status', align: 'center', label: 'Status', field: 'status', sortable: true },
-      { name: 'actions', align: 'right', label: 'Ações', field: 'actions', headerStyle: 'width: 50px', sortable: true }
+      { name: 'actions', align: 'center', label: 'Ações', field: 'actions', headerStyle: 'width: 130px', sortable: true }
     ]
   }
 }
