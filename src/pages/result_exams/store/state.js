733eaca95771ad3874639f8b18f08511b6144ee6
Waldener Monteiro <jjrr019@gmail.com>
2020-06-21 02:24:19 -0300

    refactor(result_exams): adicionando id nas colunas dos state

--- src/pages/result_exams/store/state.js
+++ src/pages/result_exams/store/state.js
@@ -2,12 +2,14 @@ export default function () {
   return {
     resultExams: [],
     columns: [
+      { name: 'id', align: 'center', label: 'Id', field: 'id', sortable: true },
       { name: 'created_at', align: 'center', label: 'Data do Resultado', field: 'created_at', sortable: true },
       { name: 'medicalSchedule', align: 'center', label: 'Médico', field: 'medicalSchedule', sortable: true },
       { name: 'patient', align: 'center', label: 'Paciente', field: 'patient', sortable: true },
       { name: 'actions', align: 'center', label: 'Ações', field: 'actions', headerStyle: 'width: 130px' }
     ],
     columnsResultExamsHistoryPatient: [
+      { name: 'id', align: 'center', label: 'Id', field: 'id', sortable: true },
       { name: 'created_at', align: 'center', label: 'Data do Resultado', field: 'created_at', sortable: true },
       { name: 'actions', align: 'center', label: 'Ações', field: 'actions', headerStyle: 'width: 130px' }
     ]
