dbd5ff533e60ee236cd3b657c2b86c8677d7a249
Waldener Monteiro <jjrr019@gmail.com>
2020-06-24 03:19:59 -0300

    refactor: alterando acesso a rotas pelos perfis corretos

--- src/router/routes.js
+++ src/router/routes.js
@@ -14,67 +14,67 @@ const routes = [
         path: '/dashboard',
         name: 'Dashboard',
         component: () => import('pages/Index.vue'),
-        meta: { roles: ['Medico', 'patient'], acess: true, name: 'Dashboard', icons: 'home' }
+        meta: { roles: ['recepcionista', 'enfermeiro', 'medico', 'paciente', 'administrador'], acess: true, name: 'Dashboard', icons: 'home' }
       },
       {
         path: '/consultas',
         name: 'Consultas',
         component: () => import('pages/medical_schedules/List.vue'),
-        meta: { roles: ['Medico', 'patient'], name: 'Consultas', icons: 'person' }
+        meta: { roles: ['administrador', 'recepcionista'], name: 'Consultas', icons: 'fas fa-book-medical' }
       },
       {
         path: '/consultas-medico',
         name: 'Consultas de Hoje',
         component: () => import('pages/medical_schedules/ListOfMedic.vue'),
-        meta: { roles: ['Medico', 'patient'], name: 'Consultas', icons: 'person' }
+        meta: { roles: ['administrador', 'medico'], name: 'Consultas', icons: 'fas fa-comment-medical' }
       },
       {
         path: '/funcionarios',
         name: 'Funcionários',
         component: () => import('pages/employees/List.vue'),
-        meta: { roles: ['Medico', 'patient'], name: 'Funcionários', icons: 'person' }
+        meta: { roles: ['administrador'], name: 'Funcionários', icons: 'fas fa-hospital-user' }
       },
       {
         path: '/medicamentos',
         name: 'Medicamentos',
         component: () => import('pages/medicaments/List.vue'),
-        meta: { roles: ['Medico', 'patient'], name: 'Medicamentos', icons: 'dashboard' }
+        meta: { roles: ['administrador'], name: 'Medicamentos', icons: 'fas fa-pills' }
       },
       {
         path: '/perfis',
         name: 'Perfis',
         component: () => import('pages/profiles/List.vue'),
-        meta: { roles: ['Medico', 'patient'], name: 'Perfis', icons: 'person' }
+        meta: { roles: ['administrador'], name: 'Perfis', icons: 'fas fa-id-card-alt' }
       },
       {
         path: '/especialidades',
         name: 'Especialidades',
         component: () => import('pages/specialities/List.vue'),
-        meta: { roles: ['Medico', 'patient'], name: 'Especialidades  ', icons: 'home' }
+        meta: { roles: ['administrador'], name: 'Especialidades  ', icons: 'fas fa-lungs' }
       },
       {
         path: '/pacientes',
         name: 'Pacientes',
         component: () => import('pages/patients/List.vue'),
-        meta: { roles: ['Medico', 'patient'], name: 'Pacientes  ', icons: 'person' }
+        meta: { roles: ['administrador', 'recepcionista'], name: 'Pacientes  ', icons: 'fas fa-head-side-cough' }
       },
       {
         path: '/tiposExames',
         name: 'Tipos de Exames',
         component: () => import('pages/types_exams/List.vue'),
-        meta: { roles: ['Medico', 'patient'], name: 'Tipos de Exames  ', icons: 'home' }
+        meta: { roles: ['administrador'], name: 'Tipos de Exames  ', icons: 'fas fa-microscope' }
       },
       {
         path: '/requisicoesExames',
         name: 'Requisições de Exames',
         component: () => import('pages/request_exams/List.vue'),
-        meta: { roles: ['Medico', 'patient'], name: 'Requisições de Exames  ', icons: 'home' }
+        meta: { roles: ['administrador', 'enfermeiro'], name: 'Requisições de Exames  ', icons: 'fas fa-file-medical' }
       },
       {
         path: '/resultadosExames',
         name: 'Resultados de Exames',
         component: () => import('pages/result_exams/List.vue'),
-        meta: { roles: ['Medico', 'patient'], name: 'Resultados de Exames  ', icons: 'home' }
+        meta: { roles: ['administrador', 'enfermeiro'], name: 'Resultados de Exames  ', icons: 'fas fa-notes-medical' }
       }
     ]
   }
