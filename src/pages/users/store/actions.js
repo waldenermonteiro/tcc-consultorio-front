517e22a6a5a2229fd8a97737fb44f7296bbd7194
Waldener Monteiro <jjrr019@gmail.com>
2020-06-24 03:19:04 -0300

    refactor(login): utilizando lowercase para capturar o nome do perfil

--- src/pages/users/store/actions.js
+++ src/pages/users/store/actions.js
@@ -5,7 +5,11 @@ export async function login ({ commit }, obj) {
     const { data } = await UserService.login(obj)
     const token = data.token.token
     const user = await decryptToken(data.user.token)
-    const role = JSON.parse(user).user.profile.name
+    let role = JSON.parse(user).user.profile.name
+    role = role
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '')
+      .toLowerCase()
     const roles = { roles: [role] }
     window.localStorage.setItem('token_clinic', token)
     window.localStorage.setItem('data_clinic', user)
@@ -14,29 +18,6 @@ export async function login ({ commit }, obj) {
     throw error
   }
 }
-
-// const login = ({ commit }, user) => {
-//   return new Promise((resolve, reject) => {
-//     commit('authRequest')
-//     HTTPClient.post('users/login', user)
-//       .then(resp => {
-//         console.log(resp)
-//         // const token = resp.data.token
-//         // window.localStorage.setItem('token_travessia_admin', token)
-//         // const data = decryptToken(token)
-//         // window.localStorage.setItem('data_travessia_admin', data)
-//         // HTTPClient.defaults.headers.common.Authorization = token
-//         // commit('authSuccess', resp.data)
-//         resolve(resp)
-//       })
-//       .catch(err => {
-//         commit('authError')
-//         localStorage.removeItem('token_travessia_admin')
-//         localStorage.removeItem('data_travessia_admin')
-//         reject(err)
-//       })
-//   })
-// }
 export const logout = async ({ commit }) => {
   window.localStorage.removeItem('token_clinic')
   window.localStorage.removeItem('data_clinic')
